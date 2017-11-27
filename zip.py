ce3b9d670c500dc93363bc45a3b37f8085a0246a
Rushi Pandya <pandyarushi@gmail.com>
2017-11-27 12:52:56 -0500

    Update zip.py
    Documentation

--- zip.py
+++ zip.py
@@ -1,3 +1,4 @@
+#this is python 2.7 version of ipython notebook. 
 import json
 import geopy
 from geopy.geocoders import Nominatim
@@ -7,7 +8,6 @@ import operator
 
 
 lis = []
-#zipcode = raw_input("Enter the zipcode from Philadelphia:")
 with open ('bikeStation.json') as j_data:
 	bikeStation = json.load(j_data)
 for i in bikeStation["features"]:
@@ -16,7 +16,7 @@ for i in bikeStation["features"]:
                 lis.append((i["geometry"]["coordinates"][0],i["geometry"]["coordinates"][1]))
 #print lis
 		
-
+#use input() in python 3.
 zipcode = raw_input("enter the zipcode:")                
 geolocator = Nominatim()
 location = geolocator.geocode(zipcode)
@@ -26,8 +26,6 @@ for i in bikeStation["features"]:
         if zipcode == i["properties"]["addressZipCode"]:
                 distance = vincenty((location.latitude,location.longitude),(i["geometry"]["coordinates"][1],i["geometry"]["coordinates"][0]))
                 lis1[str(i["properties"]["name"])] = str(distance)
-        #else:
-         #       print "no zipcode found"
 sorted_lis = sorted(lis1.items(), key = operator.itemgetter(1))
 
 for i in sorted_lis:
